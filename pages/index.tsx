5f5018707ef114365f17adad862704c55f0bccc0
vixen-cordier <vixen.cordier.13@gmail.com>
2021-04-21 19:46:22 -0400

    next/ts setup

---
+++ pages/index.tsx
@@ -0,0 +1,28 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to my Website
+        </h1>
+
+        <p className={styles.description}>
+          Get started by editing{' '}
+          <code className={styles.code}>pages/index.js</code>
+        </p>
+      </main>
+
+      <footer className={styles.footer}>
+          Powered by Vincent Codier
+      </footer>
+    </div>
+  )
+}
